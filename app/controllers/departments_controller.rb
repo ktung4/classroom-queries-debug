8e0591d2c7d390e1aae23fec9e5a1a3fe0409807
ktung4 <ktung@chicagobooth.edu>
2024-11-09 17:02:50 +0000

    Classroom debug

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -7,10 +7,11 @@ class DepartmentsController < ApplicationController
 
   def show
     the_id = params.fetch("path_id")
-    @department = Department.where({:id => the_id })
-
+    department = Department.where({:id => the_id })
+    @department = department.at(0)
     render({ :template => "departments/show" })
   end
+  
 
   def create
     @department = Department.new
